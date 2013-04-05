421182cd946201feecab5a9f721c4d81fc0b9ac0
ginok <miragino@gmail.com>
2013-04-05 19:09:47 +0900

    Implements create to Registerable

--- app/controllers/signap/registerables_controller.rb
+++ app/controllers/signap/registerables_controller.rb
@@ -3,5 +3,23 @@ module Signap
     def new
       @registerable = Registerable.new
     end
+
+    def create
+      @registerable = Registerable.new(registerable_params)
+      if @registerable.save
+        redirect_to page_after_sign_up, notice: "Success!!"
+      else
+        render :new
+      end
+    end
+
+    private
+    def registerable_params
+      params.require(:registerable).permit(:email, :password)
+    end
+
+    def page_after_sign_up
+      new_registerable_path
+    end
   end
 end
