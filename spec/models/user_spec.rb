3622bb8112b936ce0af039aadcbaf93d8ef8372c
ginok <miragino@gmail.com>
2013-04-11 18:13:30 +0900

    Refactor: spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe User do
   context 'when signing up' do
     it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:password_digest) }
     it { should allow_value('test@test.com').for(:email) }
     it { should allow_value('test+something@test.com').for(:email) }
     it { should_not allow_value('test@com').for(:email) }
@@ -14,7 +13,12 @@ describe User do
   end
 
   context 'when multiple users have signed up' do
-    before { User.create!(email: "hoge@hoge.com", password: "password") }
+    before { User.create!(email: "hoge@hoge.com") }
     it { should validate_uniqueness_of(:email) }
+  end
+
+  context 'when update' do
+    subject { User.create!(email: "hoge@hoge.com") }
+    it { should validate_presence_of(:password_digest) }
   end
 end
